0152a1c5922f61f7d0a52294bef8d0b7cf1e6e30
Adrià Huertas <adria.huertas@aetech.biz>
2023-11-13 13:21:21 +0100

    Arreglado el filtro. Ahora se puede buscar por nombre completo además de solo por nombre o apellido

--- src/utils.ts
+++ src/utils.ts
@@ -5,9 +5,7 @@ export const filterUsers = (users: UserInterface[] | null, filter: string) => {
   if (users === null) return []
   return users.filter((user) => {
     return (
-      user.first_name.toLowerCase().includes(filter.toLowerCase()) ||
-      user.last_name.toLowerCase().includes(filter.toLowerCase()) ||
-      // This last one is to search by first name + last name, otherwise it won't work if you search by fullname
+      // Search by first name + last name, otherwise it won't work if you search by fullname
       `${user.first_name} ${user.last_name}`
         .toLowerCase()
         .includes(filter.toLowerCase())
